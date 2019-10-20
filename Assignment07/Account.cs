d9210ef7ef2c528bfbf7c9a0afd68d3a12a6f516
sarker <{sarker24@gmail.com}>
2019-10-20 09:53:41 +0200

    Add to git

---
+++ Assignment07/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment07
+{
+    public class Account
+    {
+        private double Balance { get; set; }
+        public Account(double balance)
+        {
+            Balance = balance;
+        }
+
+        [ContractInvariantMethod]
+
+        public void AccountInvariant()
+        {
+            Contract.Invariant(Balance >= 0);
+        }
+
+        public double Deposite(double amount)
+        {
+            Contract.Requires<ArgumentException>(amount > 0, nameof(amount));
+            Contract.Requires<ArgumentException>(amount <= Contract.OldValue(Balance), nameof(amount));
+
+            if (amount <= 0) throw new ArgumentException($"{nameof(amount)} must be positive");
+            Balance += amount;
+            return Balance;
+        }
+
+        public double Withdrawl(double amount)
+        {
+            Contract.Requires<ArgumentException>(amount > 0, nameof(amount));
+            Contract.Requires<ArgumentException>(amount <= Contract.OldValue(Balance), nameof(amount));
+
+            if (amount <= 0) throw new ArgumentException($"{nameof(amount)} must be positive");
+
+            if (amount > Balance) throw new ArgumentException($"{nameof(amount)} is greater than {nameof(Balance)}");
+
+            Balance -= amount;
+            return Balance;
+        }
+
+
+    }
+    
+}
---
+++ Assignment07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc = new Account(1);
+            acc.Deposite(20);
+            acc.Withdrawl(100); // Invalid
+        }
+    }
+}
